9a6a160402699bf51f061ccb54c2b2611cc4600a
LYNN DAMOTA <bdamota0506@gmail.com>
2020-10-23 10:24:44 -0700

    movie and food option added

--- client/src/Components/Spots.js
+++ client/src/Components/Spots.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 
 function Spots() {
   return (
     <section className="my-5" id="spots">
-   <h1>What type of date are you feeling?</h1>
+   <h1>What type of date spot are you feeling?</h1>
   <div className="dropdownButton">
   <Dropdown>
   <Dropdown.Toggle  className="btn-warning" id="dropdown-basic">
@@ -14,15 +16,37 @@ function Spots() {
   </Dropdown.Toggle>
 
   <Dropdown.Menu alignCenter>
-    <Dropdown.Item className="dropdownItem" href="#/action-1">Action</Dropdown.Item> <br></br>
-    <Dropdown.Item className="dropdownItem"  href="#/action-2">Another action</Dropdown.Item> <br></br>
-    <Dropdown.Item className="dropdownItem"  href="#/action-3">Something else</Dropdown.Item>
+    <Dropdown.Item className="dropdownItem" href="#/action-1">Romantic</Dropdown.Item> <br></br>
+    <Dropdown.Item className="dropdownItem"  href="#/action-2">Casual</Dropdown.Item> <br></br>
+    <Dropdown.Item className="dropdownItem"  href="#/action-3">Energized</Dropdown.Item><br></br>
+    <Dropdown.Item className="dropdownItem"  href="#/action-3">Energized</Dropdown.Item><br></br>
+    <Dropdown.Item className="dropdownItem"  href="#/action-3">Spontaneous</Dropdown.Item><br></br>
+    <Dropdown.Item className="dropdownItem"  href="#/action-3">Outgoing</Dropdown.Item><br></br>
+    <Dropdown.Item className="dropdownItem"  href="#/action-3">Antisocial</Dropdown.Item><br></br>
   </Dropdown.Menu>
    </Dropdown>
   </div>
   <div className="dateSpot">
   <span className="dot"></span>
   <span className="dot"></span>
+  </div>
+  <div className="dateCards">
+      <Card cardName="movieCard" style={{ width: '18rem' }}>
+      <Card.Header>Movies</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item className="listItem">Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card>
+    <Card cardName="foodCard" style={{ width: '18rem' }}>
+    <Card.Header>Food</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card>
   </div>
   </section>
   );
