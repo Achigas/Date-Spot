81702b1219c0398c87430669254b0bf8aa4b6492
Windy <lauren.euge@gmail.com>
2020-10-25 09:01:47 -0700

    initial seach criteria, I still need to figure out how we want to separate the information into the the 3 sub categories, but I am thinking that using the search criteria in reviews for the Best Rated Spots and then the price range in Search and for the Spot with the Best Deals would be easy to add in

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,33 +2,33 @@ var APIKeyMovieDB = "4ee2048f656df52ca79c1b3928871706"
 const ZomatoAPI = "c209bc4f98e550ee1bf34b732bd7faad"
 
 export const getMe = (token) => {
-    return fetch('/api/users/me', {
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${token}`,
-      },
-    });
-  };
-  
-  export const createUser = (userData) => {
-    return fetch('/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-  };
-  
-  export const loginUser = (userData) => {
-    return fetch('/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-  };
+  return fetch('/api/users/me', {
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const createUser = (userData) => {
+  return fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const loginUser = (userData) => {
+  return fetch('/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+};
 
 //   // save book data for a logged in user
 // export const saveBook = (bookData, token) => {
@@ -41,7 +41,7 @@ export const getMe = (token) => {
 //       body: JSON.stringify(bookData),
 //     });
 //   };
-  
+
 //   // remove saved book data for a logged in user
 //   export const deleteBook = (bookId, token) => {
 //     return fetch(`/api/users/books/${bookId}`, {
@@ -53,14 +53,14 @@ export const getMe = (token) => {
 //   };
 
 //export const searchGoogleBooks = (query) => {
-    //return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-  //};
+//return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+//};
 
 export const searchNowPlayingMovies = () => {
-    //random page number
-    const pageNumber = Math.floor(Math.random() * Math.floor(5)) + 1
-    //take out API key
-    return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=4ee2048f656df52ca79c1b3928871706&language=en-US&page=${pageNumber}`)
+  //random page number
+  const pageNumber = Math.floor(Math.random() * Math.floor(5)) + 1
+  //take out API key
+  return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=4ee2048f656df52ca79c1b3928871706&language=en-US&page=${pageNumber}`)
 }
 
 export const searchPopularMovies = () => {
@@ -69,5 +69,63 @@ export const searchPopularMovies = () => {
 }
 
 export const getGenreInfo = () => {
-  return fetch ("https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKeyMovieDB + "&language=en-US")
+  return fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKeyMovieDB + "&language=en-US")
 }
+
+//HotSpot 
+// export const getHotSpot=()=>{
+//   return client search({
+//     "collection": {
+//       entity_id: 280,
+//       entity_type: city,
+//       start: 5,
+//       count: 2,
+//       collection_id: 1,
+//       user_rating: {
+//         aggregate_rating: "3.7",
+//         title: "Trending This Week",
+//       "categories": {
+//         id: 2,
+//         name: "Dine-out",
+//         }
+//       }
+//     })
+// }
+
+// //LazySpot 
+// export const getLazySpot=()=>{
+//   return client search({
+//     "collection": {
+//       entity_id: 280,
+//       entity_type: city,
+//       start: 5,
+//       count: 2,
+//     "establishment": {
+//       id: 281, 
+//       name: "Fast Food",
+//     has_online_delivery: 0,
+//     is_delivering_now: 0, 
+//     }
+//     "categories": {
+//         id: 5,
+//         name: "Takeaway",
+//         }
+//       }
+//     })
+// }
+
+// //SweetSpot
+// export const getSweetSpot=()=>{
+//   return client search{(
+//     entity_id: 280,
+//     entity_type: city,
+//     start: 5,
+//     count 2, 
+//     collection_id: 320,
+//     "establishment": {
+//     establishment_id: 31,
+//     establishment_name: "Bakery"
+//   }
+//   )}
+// }
+  
