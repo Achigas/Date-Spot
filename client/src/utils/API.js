ac702aace577344f2f60f74269d3709d9c5db0d4
Windy <lauren.euge@gmail.com>
2020-10-26 16:52:11 -0700

    clean

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -29,13 +29,13 @@ export const loginUser = (userData) => {
     body: JSON.stringify(userData),
   });
 };
-    return fetch('/api/users/me', {
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${token}`,
-      },
-    });
-  };
+  //   return fetch('/api/users/me', {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       authorization: `Bearer ${token}`,
+  //     },
+  //   });
+  // };
   
   export const createUser = (userData) => {
     return fetch('/api/users', {
@@ -91,7 +91,7 @@ export const searchNowPlayingMovies = () => {
   return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=4ee2048f656df52ca79c1b3928871706&language=en-US&page=${pageNumber}`)
     //return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   //};
-
+}
 export const searchNowPlayingMovies = () => {
     //random page number
     const pageNumber = Math.floor(Math.random() * Math.floor(5)) + 1
@@ -113,16 +113,41 @@ export const getGenreInfo = () => {
 
 //HotSpot
 export const getHotSpot = () =>{
-  return client search({
-    entity_id: 280,
-    entity_type: "city",
-    q: trending, popular, best, 
-    start: 5,
-    count: 2,
-    cuisines: "dine-in", 
-    collection_id: 1
+  return fetch('https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city', {
+  method: 'GET', //This could be any http method
+  headers: {
+    'Authorization': 'Basic c209bc4f98e550ee1bf34b732bd7faad',
+    'Content-Type': 'application/json',
+  } 
 })
+.then((response) => response.json)
+ 
+  
+  
+ // fetch('https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city')
+//   return client search({
+//     entity_id: 280,
+//     entity_type: "city",
+//     q: trending, popular, best, 
+//     start: 5,
+//     count: 2,
+//     cuisines: "dine-in", 
+//     collection_id: 1
+// })
 }
+// {
+//   entity_id: 280,
+//   entity_type: "city",
+//   q: trending, popular, best, 
+//   start: 5,
+//   count: 2,
+//   cuisines: "dine-in", 
+//   collection_id: 1,
+//   user_rating: {
+//     aggregate_rating: "3.7",
+//     title: "Trending This Week",
+//   }
+// }
 //HotSpot 
 // export const getHotSpot = () => {
 //   return client search({
@@ -180,5 +205,5 @@ export const getHotSpot = () =>{
 //   )}
 // }
   
-  return fetch ("https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKeyMovieDB + "&language=en-US")
-}
+//  return fetch ("https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKeyMovieDB + "&language=en-US")
+//}
