9237711f58ad7b775182059f6acdff530ed0223c
Rochelle Ruiz <rcelevante@gmail.com>
2020-10-27 17:14:01 -0700

    updated

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -11,18 +11,19 @@ function Login () {
       <Container className="movieOptions">
               <Card border='dark'>
                 <Card.Body>
-                <Card.Title className="movieTitle">Don't have an account yet? Sign up now! <br></br>Fill out the form below to get started. <br></br>  Already a member? <a href="#sign-in">Sign in!</a> </Card.Title>
+                <Card.Title className="movieTitle" class="title">Don't have an account yet? Sign up now! <br></br>Fill out the form below to get started. <br></br>  Already a member? <a href="#sign-in">Sign in!</a> </Card.Title>
+                <br></br>
                 <form>
-                    <label>
+                    <label class="label">
                        Username:  
                         <input type="text" class="input" name="name" />
                     </label> <br></br><br></br>
-                    <label>
+                    <label class="label">
                        Password:
                         <input type="text" class="input" name="password" /> <br></br><br></br>
                         <small>Password must be at least 8 characters.</small>
                     </label><br></br><br></br>
-                    <label>
+                    <label class="label">
                        Zipcode:
                         <input type="text" class="input" name="zipcode" />
                     </label><br></br><br></br>
@@ -30,15 +31,18 @@ function Login () {
                 </form>
                 </Card.Body>
               </Card>
+              <br></br>
+
               <Card border='dark'>
                 <Card.Body>
-                <Card.Title className="movieTitle">Sign In</Card.Title>
+                <Card.Title className="movieTitle" class="title">Sign In</Card.Title>
+                <br></br>
                 <form>
-                    <label>
+                    <label class="label" >
                        Username:
                         <input type="text" class="input" name="name" />
                     </label> <br></br><br></br>
-                    <label>
+                    <label class="label">
                        Password:
                         <input type="text" class="input" name="password" /> <br></br>
                     </label><br></br><br></br>
