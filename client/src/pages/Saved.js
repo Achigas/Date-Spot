1d17e446bce630fbb40ae4c894d73bc6ced6161f
LYNN DAMOTA <bdamota0506@gmail.com>
2020-11-01 16:53:46 -0800

    fonts updated

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -3,7 +3,7 @@ import React from "react";
 function Saved() {
   return (
     <section className="my-5" id="saved">
-   <h1 className="options">Your Saved Spots</h1>
+   <h1 className="optionsSaved">Your Saved Spots</h1>
   <div style={{
                 display: "flex",
                 justifyContent: "center",
